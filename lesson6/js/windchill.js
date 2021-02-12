04e4b49853f19e1db4d48e3c59256c9f7dbbb4aa
Alejandra-B <bla20050@byui.edu>
2021-02-12 00:40:49 -0700

    commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,11 @@
-function windchill() {
-    
+function windChill() {
+    var temp = parseFloat(document.getElementById("#temp").value);
+    var speed = parseFloat(document.getElementById("#speed").value);
+    var windchill= 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    var result = windChill(temp, speed); 
+
+    document.getElementById("#windchill").innerHTML= result + "°F"
+
 }
+
+
