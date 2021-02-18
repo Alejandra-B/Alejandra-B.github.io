41c5d3409cd6a7ea5e1da5a5eed55682412423fe
Alejandra-B <bla20050@byui.edu>
2021-02-17 23:27:32 -0700

    commit

--- lesson-7/js/lazy-load.js
+++ lesson-7/js/lazy-load.js
@@ -1,4 +1,4 @@
-const images = document.querySelectorAll("[data-src]") //This line of code finds all the images. The data-src reference all the images with lazy loading.
+const images = document.querySelectorAll("img[data-src]"); //This line of code finds all the images. The data-src reference all the images with lazy loading.
 
 
 function preloadImage(img) { //this is the function, and we give it an image for it to work. Every time we run this function we will give it something. 
@@ -9,9 +9,9 @@ function preloadImage(img) { //this is the function, and we give it an image for
     img.src = src; //if it does have a data src we want to take the image src and make it back. The first src is the actual image source, the second src is the constant we created on line 5
 }
 
-const imgOptions = { //
-    threshold: 0, // are a pass/fail criteria used to specify the performance expectations of the system under test. 
-    rootMargin: "0px 0px 300px 0px" 
+const imgOptions = { 
+    threshold: 1, // are a pass/fail criteria used to specify the performance expectations of the system under test. 
+    rootMargin: "0px 0px 50px 0px" 
 
 };
 
@@ -19,22 +19,32 @@ const imgOptions = { //
 //intersectionObserver let you know when an observed element enters or exits the browser's viewport. 
 
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {// INTERSECTION OBSERVER: tracks elements scrolling into view. 
-    entries.forEach(entry => { //this refers to each image entering the page. 
-        if (!entry.isIntersecting) {
-            return;
-        } else { //Once we preload the images we want to stop observing. 
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src")); 
+    image.onload =()=> {
+        image.removeAttribute("data-src");
 
-    })
-}, imgOptions);
+    }; 
+}; 
+if ("IntersectionObserver" in window){
+    const observer= new intersectionObserver ((items, imgObserver) =>{ //intersectionObserver tracks elements scrolling into view. 
+        items.forEach((item) => {
+            if (item.isIntersecting){
+                loadImages(item.target); 
+                imgObserver.unobserve(item.target); 
+            }
+        });
+    }, imgOptions); 
+
+imagesToLoad.forEach((img) => {
+    observer.observe(img); 
+}); 
+} else{ //once we preload the images we want to stop observing. 
+    imagesToLoad.forEach((img) =>{
+        loadImages(img); 
+    });
+}
 
 
 
-//So when we created the constant images on line 1 we created a list, but we have to apply the
-// function to each image so we have to write this line of code. 
-images.forEach(image => { //for each image inside of images, we want to run our image observer and we want to obseve that image.
-    imgObserver.observe(image);
-});
+
