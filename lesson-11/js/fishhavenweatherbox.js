c5a7563681a3a7dbe3d403ee7d69a7382a2fe832
Alejandra-B <bla20050@byui.edu>
2021-03-14 16:09:47 -0700

    commit

---
+++ lesson-11/js/fishhavenweatherbox.js
@@ -0,0 +1,29 @@
+const fishapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5585010&units=imperial&appid=33c7a61c091932d584815f44ac0838cd";
+
+//weather box
+
+
+fetch(fishapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        //these are all the variables I will need for the weather box//
+
+
+        const temperature = document.querySelector('#temp');
+        const feels_like = document.querySelector('#feelslike');
+        const humidity = document.querySelector('#humidity');
+        const current_conditions = document.querySelector("#weather");
+        const speed = document.querySelector('#speed');
+
+
+        current_conditions.textContent = jsObject.weather[0].description;
+        temperature.textContent = jsObject.main.temp;
+        feels_like.textContent = jsObject.main.feels_like;
+        speed.textContent = jsObject.wind.speed;
+        humidity.textContent = jsObject.main.humidity;
+
+        
+
+    });
