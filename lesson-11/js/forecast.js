760ad60b1b432593f1fdffcb0e3c72cb4fd0c27f
Alejandra-B <bla20050@byui.edu>
2021-03-17 16:32:34 -0700

    commit

--- lesson-11/js/forecast.js
+++ lesson-11/js/forecast.js
@@ -10,7 +10,7 @@ fetch(five_dayapiURL)
         let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
         let this_day = 0; //the current day will be on 0 index
         let five_days = jsObject.list.filter(x => x.dt_txt.includes('18:00:00')); //x:object, x.dt_text:category, 18:00:00:value
-console.log(five_days)
+console.log(five_days); 
         //loop through each of the forecast days
         five_days.forEach(jsObject => { // the api will loop for every day of the week 
             let day = new Date(jsObject.dt_txt); //this is the new day that is created from the api list
@@ -18,13 +18,14 @@ console.log(five_days)
             let imagedescription = jsObject.weather[0].description; //  reference the weather array image source description
 
             
-            
+            console.log('week_day'+this_day+1); 
+            console.log(weekday[day.getDay()]); 
             document.getElementById('week_day' + (this_day + 1)).textContent = weekday[day.getDay()];
             document.getElementById('current-temp' + (this_day + 1)).textContent = jsObject.main.temp;
             document.getElementById('icon' + (this_day + 1)).setAttribute('src', imagesrc);
             document.getElementById('icon' + (this_day + 1)).setAttribute('alt', imagedescription);
 
-        day++; //increments 1
+        this_day++; //increments 1
 
 
         });
