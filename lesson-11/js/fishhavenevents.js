a425394d98d1b2aad4a418270a3b218572201157
Alejandra-B <bla20050@byui.edu>
2021-03-17 18:26:15 -0700

    commit

--- lesson-11/js/fishhavenevents.js
+++ lesson-11/js/fishhavenevents.js
@@ -16,15 +16,24 @@ fetch(requestURL)
 
                 //variables
                 let card = document.createElement('section');
-                let events = document.createElement('h4');
+                let events = document.createElement('p');
+                let events1 = document.createElement('p');
+                let events2 = document.createElement('p');
+                let events3 = document.createElement('p');
 
                 // this is how to fill each one of the elements, this is what it will show on top on my image on the page.
                 // It also shows the order in which everything appears.
-                events.textContent = towns[i].events;
+                events.textContent = towns[i].events[0];
+                events1.textContent = towns[i].events[1];
+                events2.textContent = towns[i].events[2];
+                events3.textContent = towns[i].events[3];
 
 
                 //this is how to append the element to the card 
                 card.appendChild(events);
+                card.appendChild(events1);
+                card.appendChild(events2);
+                card.appendChild(events3);
 
                 document.querySelector('div.cards').appendChild(card);
 
--- lesson-11/js/prestonevents.js
+++ lesson-11/js/prestonevents.js
@@ -17,14 +17,19 @@ fetch(requestURL)
                 //variables
                 let card = document.createElement('section');
                 let events = document.createElement('p');
+                let events1 = document.createElement('p');
+                let events2 = document.createElement('p');
 
                 // this is how to fill each one of the elements, this is what it will show on top on my image on the page.
                 // It also shows the order in which everything appears.
-                events.textContent = towns[i].events;
-
+                events.textContent = towns[i].events[0];
+                events1.textContent = towns[i].events[1];
+                events2.textContent = towns[i].events[2];
 
                 //this is how to append the element to the card 
                 card.appendChild(events);
+                card.appendChild(events1);
+                card.appendChild(events2);
 
                 document.querySelector('div.cards').appendChild(card);
 
